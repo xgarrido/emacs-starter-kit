4f2dd1f8fe58a51e57fedc9a1a893a725ff6d5af
Xavier Garrido <xavier.garrido@gmail.com>
2013-01-13 15:54:52 +0100

    disable shift key in org mode but before loading org

--- init.el
+++ init.el
@@ -7,12 +7,16 @@
 
 ;; load the starter kit from the `after-init-hook' so all packages are loaded
 (add-hook 'after-init-hook
- `(lambda ()
-    ;; remember this directory
-    (setq starter-kit-dir
-          ,(file-name-directory (or load-file-name (buffer-file-name))))
-    ;; load up the starter kit
-    (require 'org)
-    (org-babel-load-file (expand-file-name "starter-kit.org" starter-kit-dir))))
+          `(lambda ()
+             ;; See http://orgmode.org/manual/Conflicts.html for new bindings
+             ;; and discussion why this has to be set before loading org
+             ;; http://comments.gmane.org/gmane.emacs.orgmode/53820
+             (setq org-replace-disputed-keys t)
+             ;; remember this directory
+             (setq starter-kit-dir
+                   ,(file-name-directory (or load-file-name (buffer-file-name))))
+             ;; load up the starter kit
+             (require 'org)
+             (org-babel-load-file (expand-file-name "starter-kit.org" starter-kit-dir))))
 
 ;;; init.el ends here
